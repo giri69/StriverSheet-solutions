9b4f26ae503dbbb1f97e3839c5061cc8729f8fd9
giridhar <giridharpp07@gmail.com>
2024-06-02 23:00:51 +0530

    Arrays-3

---
+++ Arrays/3-arrays-ss.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int c=0;
+        for(int i=1;i<nums.size();i++)
+        {
+            if(nums[i-1]>nums[i])
+            {
+            c++;
+            }
+        }
+        if(nums[nums.size()-1]>nums[0])c++;
+        return c<2;
+    }
+};
