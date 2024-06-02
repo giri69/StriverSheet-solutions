6d23513b2e99ee5cde52d01786631f7c162aa91b
giridhar <giridharpp07@gmail.com>
2024-06-02 08:10:27 +0530

    LeetCode 344

---
+++ Leetcode/344_Leetcode.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int f=0;int l=s.size()-1;
+        while(f<l)
+        {
+           swap(s[f++],s[l--]);
+        }
+    }
+};
