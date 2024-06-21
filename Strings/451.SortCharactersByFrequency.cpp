2bba3aef8ba4572015427bf063bb8e45cd57b42d
giridhar <giridharpp07@gmail.com>
2024-06-21 20:42:35 +0530

    451. Sort Characters By Frequency

---
+++ Strings/451.SortCharactersByFrequency.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char,int>mp;
+        for(int i=0;i<s.size();i++)mp[s[i]]++;
+        vector<pair<char,int>>v;
+        for(auto[ch,fr]:mp)v.push_back({ch,fr});
+        auto cmp = [&](pair<char,int>&a,pair<char,int>&b)
+        {
+             return a.second>b.second;
+        };
+        sort(v.begin(),v.end(),cmp);
+        string ans="";
+        for(auto x:v)
+        {
+            while(x.second--)
+            {
+                ans+=x.first;
+            }
+        }
+        return ans;
+    }
+};
+
+
