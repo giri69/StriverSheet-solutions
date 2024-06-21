6ececefff493d9f2447c70613641441f9600c45a
giridhar <giridharpp07@gmail.com>
2024-06-21 16:21:07 +0530

    242. Valid Anagram

---
+++ Strings/242.ValidAnagram.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+      unordered_map<char, int> count;
+        for (auto x : s) {
+            count[x]++;
+        }
+        for (auto x : t) {
+            count[x]--;
+        }
+        for (auto x : count) {
+            if (x.second != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
