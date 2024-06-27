4b6c84d5c0c5b317ef17927f77d45fd0ef49a970
giridhar <giridharpp07@gmail.com>
2024-06-27 21:58:14 +0530

    1791. Find Center of Star Graph

---
+++ Leetcode/1791. Find Center of Star Graph.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& edges) {
+        vector<int>a1=edges[0];
+        vector<int>a2=edges[1];
+        return (a1[0]==a2[0]||a1[0]==a2[1])?a1[0]:a1[1];
+    }
+};
