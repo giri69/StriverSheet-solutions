7794bb667a740e8ffecd9957e5ceecce52605e85
giridhar <giridharpp07@gmail.com>
2024-06-29 19:01:05 +0530

    141. Linked List Cycle

---
+++ Linked lists/141. Linked List Cycle.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+class ListNode{
+    public:
+    int val;
+    ListNode* next;
+};
+
+class Solution {
+public:
+   bool hasCycle(ListNode *head) {
+        ListNode *fast = head;
+        ListNode *slow = head;
+        while(fast != NULL && fast ->next != NULL)
+        {
+            fast = fast->next->next;
+            slow = slow->next;
+            
+            if(fast == slow)
+                return true;
+        }
+        return false;
+    }
+};
