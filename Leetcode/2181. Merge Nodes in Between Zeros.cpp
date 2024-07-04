7676bc407ada394a768c96fcaca952282b47193b
giridhar <giridharpp07@gmail.com>
2024-07-04 19:40:08 +0530

    2181. Merge Nodes in Between Zeros

---
+++ Leetcode/2181. Merge Nodes in Between Zeros.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+class ListNode{
+    public:
+    int val;
+    ListNode* next;
+};
+    
+    ListNode* mergeNodes(ListNode* head) {
+        ListNode* modify = head->next; 
+        ListNode* nextSum = modify;
+
+        while (nextSum != nullptr) {
+            int sum = 0;
+            while (nextSum->val != 0) {
+                sum += nextSum->val;
+                nextSum = nextSum->next;
+            }
+            modify->val = sum;
+            nextSum = nextSum->next;
+            modify->next = nextSum;
+            modify = modify->next;
+        }
+        return head->next; 
+    }
+};
