5c5fe560f02bc4e7f310a81c7df440d7ac6af8ae
giridhar <giridharpp07@gmail.com>
2024-07-14 20:13:48 +0530

    Representation of Graph

--- Graphs/Representation.cpp
+++ Graphs/Representation.cpp
@@ -1,2 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
+ class Solutions {  
+public:
+   vector<vector<int>> printGraph(int V, vector<pair<int,int>>edges) {
+       vector<vector<int>>arr(V);
+       for(int i=0;i<edges.size();i++)
+       {
+           int one=edges[i].first;
+           int two=edges[i].second;
+           arr[one].push_back(two);
+           arr[two].push_back(one);
+       }
+       return arr;
+    }
+    };
+
